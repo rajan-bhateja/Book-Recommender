304836f4de2efe2be8ca85f86bfc1ccde48cd5c7
rajan <rajanbhateja6@gmail.com>
2025-06-15 23:24:03 +0530

    Updated gitignore and added load_dotenv()

--- vector_search.py
+++ vector_search.py
@@ -2,7 +2,10 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.embeddings import GooglePalmEmbeddings
 from langchain_chroma import Chroma
+from dotenv import load_dotenv
 import pandas as pd
 
-books = pd.read_csv(r'books_dataset_cleaned.csv')
+load_dotenv()
+
+books = pd.read_csv('books_dataset_cleaned.csv')
 print(books['tagged_description'])
